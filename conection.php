a98d5e6c762c3c78ca13a4308383da845ee824f2
carigonz <carolinagonzalez794@gmail.com>
2019-05-26 17:44:24 -0300

    conexion ok

---
+++ conection.php
@@ -0,0 +1,17 @@
+<?php
+
+$dsn = "mysql:host=localhost;dbname=Giraff_Beer;port=3336";
+$user = "root";
+$pass = "";
+
+try {
+	$db = new PDO ($dsn, $user, $pass);
+	//para que db muestre los errores en php
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (\Exception $e) {
+	echo "hubo un error <br>";
+	
+	echo $e-getMessage();
+	exit;
+}
